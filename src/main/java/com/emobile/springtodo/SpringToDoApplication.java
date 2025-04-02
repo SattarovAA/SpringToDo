3708b841dc5c07d580421d5ff956f6cd23be23c9
SattarovAA <aa.sattarov@gmail.com>
2025-04-02 23:32:49 +0300

    some cleanup

--- src/main/java/com/emobile/springtodo/SpringToDoApplication.java
+++ src/main/java/com/emobile/springtodo/SpringToDoApplication.java
@@ -5,18 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringToDoApplication {
-    /**
-     * Возникли вопросы:
-     * com.emobile.springtodo.service.impl.UserServiceImpl#update(User)
-     * Непонятно нужно ли в таких случаях использовать self.findById(id).
-     * Откатывать транзакцию только после чтения не имеет смысла...
-     * Здесь используется для AOP logger-а.
-     * com.emobile.springtodo.service.impl.UserServiceImpl#checkDuplicateUsername(String)
-     * По большому счету подобные методы должны быть private.
-     * Откатывать транзакцию только после чтения не имеет смысла...
-     * Стоит ли их делать public ради работы @Transactional (вероятно нет).
-     * Стоит ли их делать public ради работы logger-а (вероятно нет).
-     */
     public static void main(String[] args) {
         SpringApplication.run(SpringToDoApplication.class, args);
     }
